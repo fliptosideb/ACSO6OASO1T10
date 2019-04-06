64c343dc0bb8c0ea041394f774239d3d33d03213
Annie Pham <anniekpham@yahoo.com>
2019-04-06 13:21:52 -0700

    added geolocation.js to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,43 +2,68 @@ let activities = [],
     i = 0,
     destinationsInfo = [],
     waypoints = []
-    isActivity = false,
+isActivity = false,
     isDestination = false,
     dayname,
     yourlocation,
     area,
     API_KEY = 'AxkwSE1_LXTlHlyz7rrKPjqs30_wHghk4L4k5-1w-QALR2_QM7kwVpdbNWmhnt8eFWmN-xFaIdFlaiNKRlaAGzsDHXqGzmRbt_nGrPwXPBmQYSIfq6LbgqFQpKKfXHYx';
 
+//geolocation
+const getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        yourLocation.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+const showPosition = (position) => {
+    yourLocation = document.querySelector('#yourlocation')
+
+    //reverse geocode
+    fetch(`http://www.mapquestapi.com/geocoding/v1/reverse?key=zlMKNlqjyFv79AvMHSCLunzQE5O7u7Ak&location=${position.coords.latitude},${position.coords.longitude}`)
+        .then(r => r.json())
+        .then(({ results }) => {
+            let curLocation = results[0].locations[0].street + " " + results[0].locations[0].adminArea5 + " " + results[0].locations[0].adminArea3 + " " + results[0].locations[0].postalCode
+            yourLocation.value = curLocation
+        })
+        .catch(e => console.error(e))
+}
+document.querySelector('#location').addEventListener('click', e => {
+    e.preventDefault()
+    getLocation()
+})
 const yelp = e => {
     let URL = `https://api.yelp.com/v3/businesses/search?location=${area}&term=${e}&limit=5`;
     let queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
-    fetch( queryURL, {       
+    fetch(queryURL, {
         method: "GET",
         headers: {
             "accept": "application/json",
             "x-requested-with": "xmlhttprequest",
-            "Access-Control-Allow-Origin":"*",
+            "Access-Control-Allow-Origin": "*",
             "Authorization": `Bearer ${API_KEY}`
-         }
-     })
-     .then(r => r.json())
-     .then(data => {
-         data.businesses.forEach(business => {
-            let dispbusiness = document.createElement('div')
-            dispbusiness.className = 'dispbusiness'
-            dispbusiness.coor = business.coordinates
-            dispbusiness.name = business.name
-            dispbusiness.address = business.location.display_address
-            dispbusiness.innerHTML = `
+        }
+    })
+        .then(r => r.json())
+        .then(data => {
+            data.businesses.forEach(business => {
+                let dispbusiness = document.createElement('div')
+                dispbusiness.className = 'dispbusiness'
+                dispbusiness.coor = business.coordinates
+                dispbusiness.name = business.name
+                dispbusiness.address = business.location.display_address
+                dispbusiness.innerHTML = `
             <h5>${business.name}</h5>
             <img src='${business.image_url}' class='businessimg'>
             <div>Rating: ${business.rating} Price: ${business.price}</div>
             <p class='businessaddress'>${business.location.display_address}</p>
             `
-            document.querySelector('.container').appendChild(dispbusiness)
-         })
-     })
-     .catch(e => console.error(e))
+                document.querySelector('.container').appendChild(dispbusiness)
+            })
+        })
+        .catch(e => console.error(e))
 }
 
 document.querySelector('#add').addEventListener('click', e => {
@@ -52,19 +77,19 @@ document.querySelector('#add').addEventListener('click', e => {
             let activityelem = document.createElement('span')
             activityelem.className = 'activities'
             activityelem.innerHTML = ` ${activity},`
-        
+
             document.querySelector('.plannedactivities').append(activityelem)
         })
-    } 
+    }
 })
 
 document.querySelector('.submit').addEventListener('click', e => {
     e.preventDefault()
     document.querySelector('.noactivity').innerHTML = ''
-    if (isActivity && activities.length > 0){
+    if (isActivity && activities.length > 0) {
         dayname = document.querySelector('#dayname').value,
-        yourlocation = document.querySelector('#yourlocation').value,
-        area = document.querySelector('#area').value
+            yourlocation = document.querySelector('#yourlocation').value,
+            area = document.querySelector('#area').value
         document.querySelector('.container').innerHTML = `
         <h4 class="subtitle">${activities[i]}</h4>
         `
@@ -73,18 +98,18 @@ document.querySelector('.submit').addEventListener('click', e => {
         let noactivity = document.createElement('p')
         noactivity.className = 'noactivity'
         noactivity.innerHTML = `Please add an activity`
-    
-        document.querySelector('.noactivity').append(noactivity) 
+
+        document.querySelector('.noactivity').append(noactivity)
     }
 })
 
 const nextactivity = () => {
-    if (i !== activities.length){
+    if (i !== activities.length) {
         document.querySelector('.container').innerHTML = `
         <h4 class="subtitle">${activities[i]}</h4>
         `
         yelp(activities[i])
-    } else if (i === activities.length){
+    } else if (i === activities.length) {
         document.querySelector('.container').innerHTML = `
         <h4 class='subtitle'>Your Destinations</h4>
         `
@@ -99,25 +124,25 @@ const nextactivity = () => {
         let direction = document.createElement('div')
         direction.innerHTML = `
         <button class='btn waves-effect waves-light plotcoursebtn'>Plot Course</button>
-        <button class='btn waves-effect waves-light savebtn'>Save</button>
         `
+        // <button class='btn waves-effect waves-light savebtn'>Save</button>
         direction.className = 'directionOrsave'
         document.querySelector('.container').appendChild(direction)
     }
 }
 
 document.addEventListener('click', e => {
-    if (e.target.className === 'dispbusiness' && destinationsInfo.indexOf(e.target.address) ===-1){
-        destinationsInfo.push({'name': e.target.name, 'address': e.target.address})
+    if (e.target.className === 'dispbusiness' && destinationsInfo.indexOf(e.target.address) === -1) {
+        destinationsInfo.push({ 'name': e.target.name, 'address': e.target.address })
         e.target.style.backgroundColor = '#26a69954'
         i++
-        setTimeout(nextactivity, 500) 
+        setTimeout(nextactivity, 500)
     } else if (e.target.className === 'btn waves-effect waves-light plotcoursebtn') {
         destinationsInfo.forEach(destination => {
-            waypoints.push(`${destination.address[0]} ${destination.address[1]}`)   
+            waypoints.push(`${destination.address[0]} ${destination.address[1]}`)
         })
-        document.querySelector('.container').innerHTML ='<div id="map" style="width: 100%; height: 530px;"></div>'
-        
+        document.querySelector('.container').innerHTML = '<div id="map" style="width: 100%; height: 530px;"></div>'
+
         L.mapquest.key = 'unhtsta6Q2zNmOUxHGw2VK1eiDTwNWvY';
         const render = (err, response) => {
             let map = L.mapquest.map('map', {
@@ -147,6 +172,5 @@ document.addEventListener('click', e => {
         }, render)
     } else if (e.target.className === 'btn waves-effect waves-light location') {
         e.preventDefault()
-        console.log('yay')
     }
 })
