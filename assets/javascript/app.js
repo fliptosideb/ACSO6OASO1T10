93e0e6b0ff9ecc7ca443c31bfe05810b591ebd11
Annie Pham <anniekpham@yahoo.com>
2019-04-08 06:28:08 -0700

    media query done, add map options

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,10 +3,10 @@ let activities = [],
     destinationsInfo = [],
     waypoints = []
     isActivity = false,
-    isDestination = false,
     dayname,
     yourlocation,
     area,
+    isOptimize = false,
     API_KEY = 'AxkwSE1_LXTlHlyz7rrKPjqs30_wHghk4L4k5-1w-QALR2_QM7kwVpdbNWmhnt8eFWmN-xFaIdFlaiNKRlaAGzsDHXqGzmRbt_nGrPwXPBmQYSIfq6LbgqFQpKKfXHYx'
 
 //geolocation
@@ -34,6 +34,39 @@ document.querySelector('#location').addEventListener('click', e => {
     e.preventDefault()
     getLocation()
 })
+
+// mapquest api call
+const map = t => {
+    L.mapquest.key = 'unhtsta6Q2zNmOUxHGw2VK1eiDTwNWvY';
+    const render = (err, response) => {
+        let map = L.mapquest.map('map', {
+            center: [0, 0],
+            layers: L.mapquest.tileLayer('map'),
+            zoom: 7
+        })
+        directionsLayer = L.mapquest.directionsLayer({
+            directionsResponse: response
+        }).addTo(map)
+        narrativeControl = L.mapquest.narrativeControl({
+            directionsResponse: response,
+            compactResults: false,
+            interactive: true
+        })
+        narrativeControl.setDirectionsLayer(directionsLayer);
+        narrativeControl.addTo(map)
+    }
+    L.mapquest.directions().route({
+        start: yourlocation,
+        end: yourlocation,
+        waypoints: waypoints,
+        optimizeWaypoints: t,
+        options: {
+            enhancedNarrative: true
+        }
+    }, render)
+}
+
+// yelp api call
 const yelp = e => {
     let URL = `https://api.yelp.com/v3/businesses/search?location=${area}&term=${e}&limit=6`;
     let queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
@@ -50,7 +83,7 @@ const yelp = e => {
         .then(data => {
             data.businesses.forEach(business => {
                 let dispbusiness = document.createElement('div')
-                dispbusiness.className = 'col s4 dispbusiness'
+                dispbusiness.className = 'col s12 m4 dispbusiness'
                 dispbusiness.coor = business.coordinates
                 dispbusiness.name = business.name
                 dispbusiness.address = business.location.display_address
@@ -65,10 +98,11 @@ const yelp = e => {
         })
         .catch(e => console.error(e))
 }
+
+// button to remove activity
 const activitybtns = () => {
     activities.forEach(activity => {
         let activityelem = document.createElement('span')
-        // activityelem.className = 'btn waves-effect waves-light activities'
         activityelem.innerHTML = `
         <button class='btn waves-effect waves-light rmvactivities' value='${activity}'><i class="material-icons">remove_circle</i> ${activity}</button>
         `
@@ -78,6 +112,7 @@ const activitybtns = () => {
     })
 }
 
+// add activity on click
 document.querySelector('#add').addEventListener('click', e => {
     e.preventDefault()
     if (document.querySelector('.newlocation').value) {
@@ -89,10 +124,11 @@ document.querySelector('#add').addEventListener('click', e => {
     }
 })
 
+// if input has value display yelp search else display add activity
 document.querySelector('.submit').addEventListener('click', e => {
     e.preventDefault()
     document.querySelector('.noactivity').innerHTML = ''
-    if (isActivity && activities.length > 0) {
+    if (isActivity === true) {
         dayname = document.querySelector('#dayname').value,
         yourlocation = document.querySelector('#yourlocation').value,
         area = document.querySelector('#area').value
@@ -109,13 +145,16 @@ document.querySelector('.submit').addEventListener('click', e => {
     }
 })
 
+// display yelp api for next activity on location click
 const nextactivity = () => {
     if (i !== activities.length) {
         document.querySelector('.container2').innerHTML = `
         <h4 class="subtitle">${activities[i]}</h4>
         `
         yelp(activities[i])
-    } else if (i === activities.length) {
+    } 
+    // display all destinations when location for all activites been selected
+    else if (i === activities.length) {
         document.querySelector('.container2').innerHTML = `
         <h4 class='subtitle'>Your Destinations</h4>
         `
@@ -137,48 +176,38 @@ const nextactivity = () => {
     }
 }
 
+// option to switch between exact activity order and optimize route
+const direction = () => {
+    if (isOptimize === false) {
+        document.querySelector('.container').innerHTML = ''
+        document.querySelector('.container').innerHTML = `
+        <button class="btn waves-effect waves-light isOptimize" type="submit">Optimize Route</button>
+        <div id="map" style="width: 100%; height: 530px;"></div>
+        `
+        map(false)
+    } else if (isOptimize === true) {
+        document.querySelector('.container').innerHTML = ''
+        document.querySelector('.container').innerHTML = `
+        <button class="btn waves-effect waves-light isOptimize" type="submit">Exact Stop</button>
+        <div id="map" style="width: 100%; height: 530px;"></div>
+        `
+        map(true)
+    }
+}
+
 document.addEventListener('click', e => {
-    if (e.target.className === 'col s4 dispbusiness' && destinationsInfo.indexOf(e.target.address) === -1) {
+    if (e.target.className === 'col s12 m4 dispbusiness' && destinationsInfo.indexOf(e.target.address) === -1) {
         destinationsInfo.push({ 'name': e.target.name, 'address': e.target.address })
-        // e.target.style.backgroundColor = '#26a69954'
         i++
         setTimeout(nextactivity, 500)
     } else if (e.target.className === 'btn waves-effect waves-light plotcoursebtn') {
         destinationsInfo.forEach(destination => {
             waypoints.push(`${destination.address[0]} ${destination.address[1]}`)
         })
-        document.querySelector('.container2').innerHTML = '<div id="map" style="width: 100%; height: 530px;"></div>'
-
-        L.mapquest.key = 'unhtsta6Q2zNmOUxHGw2VK1eiDTwNWvY';
-        const render = (err, response) => {
-            let map = L.mapquest.map('map', {
-                center: [0, 0],
-                layers: L.mapquest.tileLayer('map'),
-                zoom: 7
-            })
-            directionsLayer = L.mapquest.directionsLayer({
-                directionsResponse: response
-            }).addTo(map)
-            narrativeControl = L.mapquest.narrativeControl({
-                directionsResponse: response,
-                compactResults: false,
-                interactive: true
-            })
-            narrativeControl.setDirectionsLayer(directionsLayer);
-            narrativeControl.addTo(map)
-        }
-        L.mapquest.directions().route({
-            start: yourlocation,
-            end: yourlocation,
-            waypoints: waypoints,
-            optimizeWaypoints: true,
-            options: {
-                enhancedNarrative: true
-            }
-        }, render)
+        direction()
     } else if (e.target.className === 'btn waves-effect waves-light location') {
         e.preventDefault()
-    } else if (e.target.className === 'btn waves-effect waves-light rmvactivities') {
+    } else if (e.target.className === 'btn waves-effect waves-light rmvactivities' || e.target.className === 'material-icons') {
         e.preventDefault()
         document.querySelector('.plannedactivities').innerHTML = ''
         let j = activities.indexOf(e.target.value)
@@ -186,5 +215,9 @@ document.addEventListener('click', e => {
             activities.splice(j,1)
         }
         activitybtns()
+    } else if (e.target.className === 'btn waves-effect waves-light isOptimize') {
+        e.preventDefault()
+        isOptimize = !isOptimize
+        direction()
     }
 })
